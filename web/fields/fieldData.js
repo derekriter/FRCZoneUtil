765aab7bbe67f899ab02b597d7f56275941e05a8
Derek Riter <derekriter08@gmail.com>
2025-03-31 17:01:51 -0400

    Working on field, gettings CORS eror

---
+++ web/fields/fieldData.js
@@ -0,0 +1,13 @@
+export const reefscape2025 = {
+    "image": "fieldImages/Reefscape2025.svg",
+    "size": {
+        "x": 17.54823,
+        "y": 8.0518
+    },
+    "boundingBox": {
+        "x1": 3549.59,
+        "y1": 13064.39,
+        "x2": 28450.61,
+        "y2": 1639.81
+    }
+}
---
+++ web/main.js
@@ -0,0 +1,29 @@
+function handleError(code, consoleMsg) {
+    console.error(`Err ${code}: ${consoleMsg}`);
+
+    alert(`An error occurred. Please reload the page and try again.\n\nErr ${code}: ${consoleMsg}`);
+    window.location.reload(); //force reload the page after the user presses ok
+}
+function getElementOrErr(id, code, consoleMsg) {
+    let elem = document.getElementById(id);
+
+    if(elem === null) {
+        handleError(code, consoleMsg);
+        return null; //should never make it here but I have it anyway
+    }
+
+    return elem;
+}
+
+function loadJSONFile(path) {
+    fetch(path)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+function setupField(field) {
+    console.log(reefscape2025);
+}
+
+const fieldImg = getElementOrErr("fieldImg", 0, "Failed to get fieldImg element");
+let fieldData;
+setupField(fieldImg);
