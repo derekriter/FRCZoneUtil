38022bb9fa298a89c903dcf0062e7bbfcaa7c25f
Derek Riter <derekriter08@gmail.com>
2025-03-31 17:23:27 -0400

    Move to using electron, still need to adapt code

--- web/main.js
+++ web/main.js
@@ -1,3 +1,17 @@
+const {app, BrowserWindow} = require("electron");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    win.loadFile("index.html");
+};
+
+app.whenReady()
+    .then(() => createWindow());
+
 function handleError(code, consoleMsg) {
     console.error(`Err ${code}: ${consoleMsg}`);
 
@@ -21,7 +35,7 @@ function loadJSONFile(path) {
         .then((json) => console.log(json));
 }
 function setupField(field) {
-    console.log(reefscape2025);
+    // console.log(reefscape2025);
 }
 
 const fieldImg = getElementOrErr("fieldImg", 0, "Failed to get fieldImg element");
