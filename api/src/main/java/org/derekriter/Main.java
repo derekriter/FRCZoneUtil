262209c9b323119a3d19d95f056e24f5400644f8
Derek Riter <derekriter08@gmail.com>
2025-04-18 17:59:37 -0400

    Start json loading

--- api/src/main/java/org/derekriter/Main.java
+++ api/src/main/java/org/derekriter/Main.java
@@ -1,7 +1,22 @@
 package org.derekriter;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public final class Main {
+    public static void main(String[] args) throws IOException {
+        //TODO: replace file reading
+        final InputStream stream = Main.class.getResourceAsStream("/map.json");
+        if(stream == null) {
+            throw new IOException("Failed to open json file");
+        }
+
+        final Scanner scanner = new Scanner(stream);
+        while(scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+
+        scanner.close(); //closes file too
     }
 }
